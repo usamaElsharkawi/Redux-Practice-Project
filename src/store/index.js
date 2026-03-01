2fb150228d67fab6290c356fa1d7d6e9aa09c8f1
Usama El-Sharkawi <usamasharkawi@gmail.com>
2026-03-01 10:53:23 +0200

    Providing the Store

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore } from "redux";
+
+
+const initialState = {
+    counter: 0,
+};
+
+const reducer = (state = initialState, action) => {
+    if (action.type === "INCREMENT") {
+        return {
+            counter: state.counter + 1,
+        };
+    }
+    if (action.type === "DECREMENT") {
+        return {
+            counter: state.counter - 1,
+        };
+    }
+    return state;
+};
+
+const store = createStore(reducer);
+
+export default store;
