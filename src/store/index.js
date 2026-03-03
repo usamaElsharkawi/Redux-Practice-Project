8650d04322578a4db6995678873b1608311ff1fb
Usama El-Sharkawi <usamasharkawi@gmail.com>
2026-03-03 11:43:06 +0200

    Adding State Slices & Connecting Redux Toolkit State

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore } from "redux";
+import { createSlice,configureStore } from "@reduxjs/toolkit";
 
 
 const initialState = {
@@ -6,34 +6,29 @@ const initialState = {
     showCounter: true,
 };
 
-const reducer = (state = initialState, action) => {
-    if (action.type === "INCREMENT") {
-        return {
-            ...state,
-            counter: state.counter + 1,
-        };
-    }
-    if (action.type === "DECREMENT") {
-        return {
-            ...state,
-            counter: state.counter - 1,
-        };
-    }
-    if (action.type === "INCREASE") {
-        return {
-            ...state,
-            counter: state.counter + action.payload,
-        };
-    }
-    if (action.type === "TOGGLE") {
-        return {
-            ...state,
-            showCounter: !state.showCounter,
-        };
-    }
-    return state;
-};
+const counterSlice = createSlice({
+    name: "counter",
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter += action.payload;
+        },
+        toggle(state) {
+            state.showCounter = !state.showCounter;
+        },
+    },
+});
 
-const store = createStore(reducer);
+
+
+const store = configureStore({
+    reducer: counterSlice.reducer,
+});
 
 export default store;
