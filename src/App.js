a8be55be67587fb0af4863a751dc01e7444346b3
Usama El-Sharkawi <usamasharkawi@gmail.com>
2026-03-03 23:08:18 +0200

    Reading & Dispatching From A New Slice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   return (
     <>
     <Header />
-    <Auth />
-    <Counter />
+    {!isAuthenticated && <Auth />}
+    {isAuthenticated && <UserProfile />}
+    {isAuthenticated && <Counter />}
     </>
   );
 }
